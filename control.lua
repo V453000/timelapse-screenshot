a223c3969f801325cdf78dfa93703229243540f5
Vaclav Benc <vaclav.benc@factorio.com>
2020-04-19 21:14:40 +0200

    WIP Making the resolution smarter, cleanup.

--- control.lua
+++ control.lua
@@ -1,6 +1,8 @@
-local timelapse_screenshot_interval_ticks = settings.startup['timelapse-screenshot-interval-ticks'].value
+local setting_timelapse_screenshot_interval_ticks = settings.startup['timelapse-screenshot-interval-ticks'].value
+local setting_max_resolution_x = settings.startup['timelapse-screenshot-max-resolution-x'].value
+local setting_max_resolution_y = settings.startup['timelapse-screenshot-max-resolution-y'].value
 
-function time_format(time_int)
+local function time_format(time_int)
   local hours_int = math.floor(time_int/60/60/60)
   local remaining_ticks_minutes = time_int - hours_int*60*60*60
   local minutes_int = math.floor(remaining_ticks_minutes/60/60)
@@ -43,90 +45,79 @@ function time_format(time_int)
   return time_string
 end
 
-function move_entities(name_list, move_x, move_y, search_area)
-  local logo_entities = game.surfaces['nauvis'].find_entities_filtered({area = search_area, name = name_list})
-  for _, e in pairs(logo_entities) do
-    e.teleport({e.position.x+move_x, e.position.y+move_y})
-  end  
+local function get_surface_size_from_center(surface)
+  local range_x = 0
+  local range_y = 0
+
+  for chunk in surface.get_chunks() do
+    local chunk_x = math.abs(chunk.x)
+    local chunk_y = math.abs(chunk.y)
+    
+    if chunk_x > range_x then
+      range_x = math.abs(chunk.x)
+    end
+    if chunk_y > range_y then
+      range_y = math.abs(chunk.y)
+    end
+  end
+
+  local range = {}
+  range.x = range_x
+  range.y = range_y
+  return range
 end
 
-function timelapse_screenshot(tick)
-  local seed = game.surfaces['nauvis'].map_gen_settings.seed
-  game.print(seed)
-  local arg_filename_base = 'seed'
-  local arg_position = {20, -46}
+local function timelapse_screenshot(tick)
+  local surf = game.surfaces['nauvis']
+  local seed = surf.map_gen_settings.seed
+  local arg_filename_base = seed
+  local arg_position = {0, 0}
+  local arg_zoom = settings.startup['timelapse-screenshot-zoom'].value
+  
+  
   local tile_px = 32
   local chunk_tiles = 32
-  local arg_zoom = 1
-  local resolution_multiplier = 1/2
+  local surface_size = get_surface_size_from_center(surf)
+  local resolution_multiplier = 1 * arg_zoom
 
-  if tick > 1 *60*60*60 then
-    arg_zoom = 1/2
+  local resolution_candidate_x = resolution_multiplier * tile_px * chunk_tiles * surface_size.x
+  local resolution_candidate_y = resolution_multiplier * tile_px * chunk_tiles * surface_size.y
+
+  if resolution_candidate_x > setting_max_resolution_x then
+    local resolution_divider = math.ceil(resolution_candidate_x / setting_max_resolution_x)
+    resolution_multiplier = resolution_multiplier / resolution_divider
   end
-  if tick > 9 *60*60*60 then
-    arg_zoom = 1/4
-  end
-  if tick > 30 *60*60*60 then
-    arg_zoom = 1/8
+  if resolution_candidate_y > setting_max_resolution_y then
+    local resolution_divider = math.ceil(resolution_candidate_x / setting_max_resolution_x)
+    resolution_multiplier = resolution_multiplier / resolution_divider
   end
 
-  local arg_resolution = {16*resolution_multiplier*tile_px*chunk_tiles, 9*resolution_multiplier*tile_px*chunk_tiles}
+  local resolution_x = resolution_multiplier * tile_px * chunk_tiles * surface_size.x
+  local resolution_y = resolution_multiplier * tile_px * chunk_tiles * surface_size.y
 
-  local timelapse_subfolder = 'test/'
-  local arg_path = 'built-in-timelapse/' .. timelapse_subfolder .. arg_filename_base .. '_' .. time_format(tick) .. '.png'
+  local arg_resolution = { resolution_x, resolution_y }
+
+  local timelapse_subfolder = seed .. '/'
+  local arg_path = 'timelapse-screenshot/' .. timelapse_subfolder .. arg_filename_base .. '_' .. time_format(tick) .. '.png'
   game.take_screenshot{path = arg_path, position = arg_position, resolution = arg_resolution, zoom = arg_zoom, render_tiles = true};
+
+  local data = {}
+  data.resolution_multiplier = resolution_multiplier
+  data.setting_max_resolution_x = setting_max_resolution_x
+  data.setting_max_resolution_y = setting_max_resolution_y
+  data.resolution_x = resolution_x
+  data.resolution_y = resolution_y
+  return data
 end
 
-
--- prepare
-script.on_init(
-  function()
-    --move logo entities
-    local logo_name_list =
-    {
-      'factorio-logo-0',
-      'factorio-logo-1',
-      'factorio-logo-2',
-      'factorio-logo-3',
-      'factorio-logo-4',
-      'factorio-logo-5',
-      'factorio-logo-6',
-    }
-    move_entities( logo_name_list, 0, 5, {left_top = {-15.5-1, -41.5-1}, right_bottom = {56.5+1, -41.5+1}} )
-    -- change player colour
-    for p, player in pairs(game.players) do
-      player.color = { r = 0.869, g = 0.5, b = 0.130, a = 0.5 };
-    end
-
-    for s, surface in pairs(game.surfaces) do
-      -- set day
-      surface.daytime = 1;
-      -- chart a minimum area
-      surface.request_to_generate_chunks({0,0}, 9);
-      surface.force_generate_chunk_requests();
-    end
-
-    for f, force in pairs(game.forces) do
-      force.chart_all();
-    end
-  end
-)
-
-script.on_event(defines.events.on_player_joined_game,
-  function()  
-    -- change player colour
-    for p, player in pairs(game.players) do
-      player.color = { r = 0.869, g = 0.5, b = 0.130, a = 0.5 };
-    end
-  end
-)
 
 -- take screenshot
 script.on_event(defines.events.on_tick,
   function()
-    if timelapse_screenshot_interval_ticks > 0 then
-      if game.tick % (timelapse_screenshot_interval_ticks) == 0 then
-        timelapse_screenshot(game.tick)
+    if setting_timelapse_screenshot_interval_ticks > 0 then
+      if game.tick % (setting_timelapse_screenshot_interval_ticks) == 0 then
+        local data = timelapse_screenshot(game.tick)
+        --log(serpent.block(data))
       end
     end
   end
--- settings.lua
+++ settings.lua
@@ -3,8 +3,31 @@ data:extend{
     type = "int-setting",
     name = "timelapse-screenshot-interval-ticks",
     setting_type = "startup",
-    default_value = 60*60*30,
+    default_value = 25000,--1 factorio day--60*60*30,
     min_value = 1,
     order = "a"
+  },
+  {
+    type = "double-setting",
+    name = "timelapse-screenshot-zoom",
+    setting_type = "startup",
+    default_value = 0.25,
+    order = "b"
+  },
+  {
+    type = "int-setting",
+    name = "timelapse-screenshot-max-resolution-x",
+    setting_type = "startup",
+    default_value = 4096,
+    max_value = 16384,
+    order = "c-a"
+  },
+  {
+    type = "int-setting",
+    name = "timelapse-screenshot-max-resolution-y",
+    setting_type = "startup",
+    default_value = 4096,
+    max_value = 16384,
+    order = "c-b"
   },
 }
